1b2b5627806c699461853579ab711162c90789f9
Dr James V Stone <j.v.stone@sheffield.ac.uk>
2019-03-20 10:52:36 +0000

    Update main.py

--- DeepLearningEnginesCode/Python/Ch03_Perceptron/main.py
+++ DeepLearningEnginesCode/Python/Ch03_Perceptron/main.py
@@ -1,103 +1,120 @@
-#!/usr/bin/env python
-#coding: utf-8
-# modified by JVStone, was test.py
-# JVS put all code in this file.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Perceptron
+@author: JimStone
+"""
 
-# local module
-# import generate_data
-#import perceptron
 import matplotlib.pyplot as plt
 import numpy as np
 
 # set random seed so get same sequence of random numbers each time prog is run.
-np.random.seed(2)
+# np.random.seed(20)
 
 class Perceptron(object):
     """docstring for Perceptron"""
     def __init__(self):
         super(Perceptron, self).__init__()
         self.w = [np.random.rand() * 2 - 1 for _ in range(2)] # weights
-        self.learningRate = 0.1
+        self.learningRate = 0.001
 
     def response(self, x):
-        """perceptron output"""
-        # y = x[0] * self.w[0] + x[1] * self.w[1] # dot product between w and x
-        y = sum([i * j for i, j in zip(self.w, x)]) # more pythonic
+        # perceptron output
+        y = x[0] * self.w[0] + x[1] * self.w[1] # dot product between w and x
+        # y = sum([i * j for i, j in zip(self.w, x)]) # more pythonic
+        #y = np.dot(x[0:1],self.w)
+        y=y*1.0
         if y >= 0:
-            return 1
+            res = 1.0
         else:
-            return -1
-
+            res = -1.0        
+        return res
+    
     def updateWeights(self, x, iterError):
         """
         upates the wights status, w at time t+1 is
         w(t+1) = w(t) + learningRate * (d - r) * x
         iterError is (d - r)
         """
-        # self.w[0] += self.learningRate * iterError * x[0]
-        # self.w[1] += self.learningRate * iterError * x[1]
-        self.w = \
-            [i + self.learningRate * iterError * j for i, j in zip(self.w, x)]
+        self.w[0] += self.learningRate * iterError * x[0]
+        self.w[1] += self.learningRate * iterError * x[1]
+        #self.w = \
+            #[i + self.learningRate * iterError * j for i, j in zip(self.w, x)]
 
     def train(self, data):
         """
-        trains all the vector in data.
+        Trains all the vector in data.
         Every vector in data must three elements.
-        the third eclemnt(x[2]) must be the label(desired output)
+        The third element of x (ie x[2]) is the label(desired output)
         """
         learned = False
         iteration = 0
         while not learned:
+            numcorrect = 0
             globalError = 0.0
-            for x in data: # for each sample
+            for x in data: # for each input vector
                 r = self.response(x)
                 if x[2] != r: # if have a wrong response
                     iterError = x[2] - r # desired response - actual response
                     self.updateWeights(x, iterError)
                     globalError += abs(iterError)
+                else:
+                    numcorrect += 1
+            print('num correctly classified = %s' % numcorrect)
             iteration += 1
             if globalError == 0.0 or iteration >= 100: # stop criteria
-                print('iterations: %s' % iteration)
-                learned = True # stop learing
+                print('iterations = %s' % iteration)
+                learned = True # stop learning
+                
+            ########## Plot ##########
+            plotinterval = 2
+            if (iteration % plotinterval == 0):
+                plotData(data,self.w)
+                plt.pause(0.001)
 
 def generateData(n):
     """
-    generates a 2D linearly separable dataset with n samples.
-    The thired element of the sample is the label
+    generates a 2D linearly separable dataset with 2n samples.
+    The third element of the sample is the correct response.
     """
-    xb = (np.random.rand(n) * 2 -1) / 2 - 0.5
-    yb = (np.random.rand(n) * 2 -1) / 2 + 0.5
-    xr = (np.random.rand(n) * 2 -1) / 2 + 0.5
-    yr = (np.random.rand(n) * 2 -1) / 2 - 0.5
+    # class A=blue dots has all y values above 0
+    xb = (np.random.rand(n) * 2 -1)  
+    yb = (np.random.rand(n) * 2 -1) / 2 + 0.55
+    
+    # class B=red dots has all y values below 0
+    xr = (np.random.rand(n) * 2 -1) 
+    yr = (np.random.rand(n) * 2 -1) / 2 - 0.55
+    
     inputs = []
     inputs.extend([[xb[i], yb[i], 1] for i in range(n)])
     inputs.extend([[xr[i], yr[i], -1] for i in range(n)])
     return inputs
 
+def plotData(data,w):
+    plt.clf()
+    for x in data:
+        if x[1] < 0:
+            plt.plot(x[0], x[1], 'ob')
+        else:
+            plt.plot(x[0], x[1], 'or')
+    # plot the decision boundary. 
+    # The decision boundary is orthogonal to w.
+    n = np.linalg.norm(w) # aka the length of p.w vector
+    ww = w / n # a unit vector
+    ww1 = [ww[1], -ww[0]]
+    ww2 = [-ww[1], ww[0]]
+    plt.ion()
+    plt.plot([ww1[0], ww2[0]], [ww1[1], ww2[1]], '--k')
+    plt.title('Two classes separated by a line orthogonal to the weight vector')
+    plt.show()
+    
 trainset = generateData(80) # train set generation
-testset = generateData(20) # test set generation
-p = Perceptron() # use a short
+# testset = generateData(20) # test set generation
+
+p = Perceptron() 
+
+plotData(trainset,p.w)
 p.train(trainset)
+plotData(trainset,p.w)
 
-#Perceptron test
-for x in testset:
-    r = p.response(x)
-    if r != x[2]: # if the response is not correct
-        print('not hit.')
-    if r == 1:
-        plt.plot(x[0], x[1], 'ob')
-    else:
-        plt.plot(x[0], x[1], 'or')
-
-# plot of the separation line. 
-# The centor of line is the coordinate origin
-# So the length of line is 2
-# The separation line is orthogonal to w
-n = np.linalg.norm(p.w) # aka the length of p.w vector
-ww = p.w / n # a unit vector
-ww1 = [ww[1], -ww[0]]
-ww2 = [-ww[1], ww[0]]
-plt.ion()
-plt.plot([ww1[0], ww2[0]], [ww1[1], ww2[1]], '--k')
-plt.title('Two classes separated by a line orthogonal to the weight vector')
-plt.show()
+########## The End ##########
